1d7eb8d88dbdc52058438be5a8c23a3acf514b1f
Paxal Shah <paxalshah31@gmail.com>
2022-07-29 13:37:30 -0400

    Updates

---
+++ precache-manifest.40f105165e537362cbf7b9223cf90575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d357d68e0f716311c97",
+    "url": "/robofriends-react/static/css/main.25256a62.chunk.css"
+  },
+  {
+    "revision": "7d357d68e0f716311c97",
+    "url": "/robofriends-react/static/js/main.fe712fd9.chunk.js"
+  },
+  {
+    "revision": "f49965f98b8f923c19bf",
+    "url": "/robofriends-react/static/js/runtime~main.7d34a06e.js"
+  },
+  {
+    "revision": "fcb1ff8c5e357316ea02",
+    "url": "/robofriends-react/static/css/2.39b659c2.chunk.css"
+  },
+  {
+    "revision": "fcb1ff8c5e357316ea02",
+    "url": "/robofriends-react/static/js/2.02448105.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/robofriends-react/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "7dce718701d37b395bdbede0027cbc8a",
+    "url": "/robofriends-react/index.html"
+  }
+];
